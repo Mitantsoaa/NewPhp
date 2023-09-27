e7d953c0e73d26abcf233822b55f85d1f1a0a3fb
Mitantsoaa <mitaranaivo@gmail.com>
2023-09-27 22:06:16 +0300

    Petit pratique avec les formulaires

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
-<?php
-if(isset($_POST['nom'])){
-    echo "Hello ".$_POST['nom'];
-}
-    ?>
+
 
     <form action="./index.php" method="POST">
         <input type="text" name="nom" placeholder="entrer votre nom">
         <input type="text" name="telephone" placeholder="entrer votre phone">
         <input type="text" name="cin" placeholder="entrer votre cin">
+        Homme:<input type="radio" name="gender" value="H">
+        Femme:<input type="radio" name="gender" value="F">
+        Paoma:<input type="checkbox" name="fruits[]" value="Paoma">
+        Paiso:<input type="checkbox" name="fruits[]" value="Paiso">
+        Poara:<input type="checkbox" name="fruits[]" value="Poara">
+        Voasary:<input type="checkbox" name="fruits[]" value="Voasary">
+        Akondro:<input type="checkbox" name="fruits[]" value="Akondro">
         <input type="submit" value="Save">
     </form>
 
@@ -24,4 +27,19 @@ if(isset($_POST['nom'])){
             <td>Cin:</td>
             <td><?php  if(isset($_POST['cin'])){echo $_POST['cin'];} ?></td>
         </tr>
+        <tr>
+            <td>Genre:</td>
+            <td><?php  if(isset($_POST['cin'])){echo $_POST['gender'];} ?></td>
+        </tr>
+        <tr>
+            <td>Fruits:</td>
+            <td><?php  if(isset($_POST['fruits'])){
+                $voa = '';
+                foreach($_POST['fruits'] as $fruit){
+                    $voa .= $fruit. ', ';
+                }
+                echo $voa;
+            } ?></td>
+        </tr>
     </table>
+
