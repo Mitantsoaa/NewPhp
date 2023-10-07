76e1d1872378bc0d024ce63dd7138df562372f29
Mitantsoaa <mitaranaivo@gmail.com>
2023-10-07 21:43:46 +0300

    Amelioration kely sahady

--- validationNom.php
+++ validationNom.php
@@ -3,13 +3,13 @@
 declare(strict_types=1);
     function getFullName(string $nom, ?string $prenom = ''): string
     {
-        if(preg_match ("/^[a-zA-z-é.]*$/", $nom) && isset($prenom) && preg_match("/^[a-zA-z-é.]*$/", $prenom)){
+        if(preg_match ("/^[a-zA-z-é.ôêè]*$/", $nom) && isset($prenom) && preg_match("/^[a-zA-z-é.ôêè]*$/", $prenom)){
             return "Votre nom complet est: $nom $prenom";
-        }else if(preg_match ("/^[a-zA-z-é.]*$/", $nom)){
+        }else if(preg_match ("/^[a-zA-z-é.ôêè]*$/", $nom)){
              return "Votre nom complet est: $nom";
         }else{
             return "Nom invalide";
         }
     }
-echo getFullName('Rakoto.koto=','be');
+echo getFullName('Rakoto.kotô','joêl');
 // [a-zA-Z-. accent ]
